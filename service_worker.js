1d0585ab8d6a6d51a0e92d2628ff7db7b1d424c8
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2020-04-25 14:39:43 +0300

    add service worker

---
+++ service_worker.js
@@ -0,0 +1,52 @@
+"use strict";
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll([
+                './index.html',
+                './style.css',
+                './index.js',
+                './index.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.open('v1').then(async function(cache) {
+            const cache_response = await cache.match(e.request);
+            if (cache_response) {
+                return cache_response;
+            }
+            else {
+                const response = fetch(e.request);
+                await cache.put(e.request, (await response).clone());
+                return response;
+            }
+        })
+    );
+    // e.respondWith(
+    //     caches.open('v1').then(function(cache) {
+    //         return cache.match(e.request);
+    //     })
+    // );
+
+    // e.waitUntil(
+    //     caches.open('v1').then(async function(cache) {
+    //         const response = await fetch(e.request);
+    //         await cache.put(e.request, response.clone());
+    //         const clients = await self.clients.matchAll();
+    //         const message = JSON.stringify({
+    //             type: 'refresh',
+    //             url: response.url,
+    //             eTag: response.headers.get('ETag'),
+    //         });
+    //         clients.forEach(function(client) {
+    //             client.postMessage(message);
+    //         });
+    //         return response;
+    //     })
+    // )
+});
